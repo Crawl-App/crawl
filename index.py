0630c129636a3a34bab07164b2921df9c168085e
Steph Dunn <sdun8682@uni.sydney.edu.au>
2024-08-24 23:08:01 +1000

    Add documentation for API endpoint

--- index.py
+++ index.py
@@ -10,9 +10,38 @@ def generate_pub_crawl():
     """
     Flask endpoint to find a pub crawl route based on the provided coordinates, length, and use_current_loc flag.
     Expected JSON body: { "coordinates": [lat, lng], "length": number_of_stops, "use_current_loc": true/false }
+      - coordinates: latitude and longitude of starting position (for when current location is not being used)
+      - length: number of stops on crawl, positive integer
+      - use_current_loc: boolean describing whether or not to use current location. if set to false, coordinates
+      of starting position must be specified.
+    
+    Returns:
+     - A JSON object in the form:
+        {
+            "number_of_stops": n,
+            "route": [
+                [
+                    "start",
+                    lat,
+                    lng
+                ],
+                [
+                    "destination_1",
+                    lat,
+                    lng
+                ],
+                ...
+                [
+                    "destination_n",
+                    lat,
+                    lng
+                ]
+            ],
+            "start_location": [lat, lng]
+            "use_current_loc": true/false
+        }
     """
     # Initialise client based off API key in environment variables
-    # load_dotenv()
     api_key = os.environ.get('GOOGLE_MAPS_API_KEY')
     print(api_key)
 
@@ -24,6 +53,9 @@ def generate_pub_crawl():
     coordinates = (data.get('coordinates')[0], data.get('coordinates')[1])
     length = data.get('length')
     use_current_loc = data.get('use_current_loc', False)  # Default to False if not provided
+    
+    if not use_current_loc and not data.get('coordinates'):
+        raise ValueError("coordinates must be specified if not using current location.")
 
     if not coordinates or not length:
         return jsonify({"error": "Coordinates and length are required"}), 400
@@ -37,7 +69,7 @@ def generate_pub_crawl():
         "use_current_loc": use_current_loc,
         "route": find_pub_crawl(gmaps, coordinates, length, use_current_loc)
     }
-
+    
     return jsonify(pub_crawl_result)
 
 if __name__ == '__main__':
